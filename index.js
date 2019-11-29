8b3944dfd0d715611c38d070c54ec9beaed9b37f
ochoi <ochoi@terpmail.umd.edu>
2019-11-29 17:05:34 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// Modules
+var express = require('express');
+var bodyParser = require('body-parser');
+var dataUtil = require("./data-util");
+var _ = require("underscore");
+var logger = require('morgan');
+var exphbs = require('express-handlebars');
+var handlebars = exphbs.handlebars;
+var moment = require('moment');
+var marked = require('marked');
+var app = express();
+var PORT = 8000;
+
+
+// MIDDLEWARE 
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.engine('handlebars', exphbs({ defaultLayout: 'main', partialsDir: "views/partials/" }));
+app.set('view engine', 'handlebars');
+app.use('/public', express.static('public'));
+
+
+// Default Endpoint
+app.get("/", function(req, res) {
+    
+});
